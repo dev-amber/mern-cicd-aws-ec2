02e648783e0edc18622f2ac7b4d1079071fb4def
Amber naz <ambernaz1000@gmail.com>
2026-08-05 18:40:18 +0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
      <header className="App-header">
-       <h1>CI/CD pipelines with Github actions</h1>
+       <h1>CI/CD pipelines with Github actions now</h1>
       </header>
 
       </div>
