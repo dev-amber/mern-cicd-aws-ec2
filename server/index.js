66705a02e26621adc10663f897932197e0a49553
dev-amber <ambernaz1000@gmail.com>
2026-08-05 17:51:57 +0500

    All files

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from Express Backend!",
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
